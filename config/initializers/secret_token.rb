011babe2b05d531c247e36f5580c96fef0313235
lukemesiti <lukemesiti@gmail.com>
2014-03-23 17:58:10 +1100

    first commit. created rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vicetracker::Application.config.secret_key_base = '5795baf63965feff230f38d03bb4194438c4034c229b28b5ade4595ee61142f2857ab27bbc3393ddb6ba933c7a5190ee9af9822a66c0fa61a1fbc2ac4b32ea3f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Vicetracker::Application.config.session_store :cookie_store, key: '_vicetracker_session'
