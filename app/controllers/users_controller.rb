7b5d6215f02e85f9680a133677763136c0d1b551
lukemesiti <lukemesiti@gmail.com>
2014-04-11 10:31:39 +1000

    updated sanitized parameters for user update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
     end
 
     def user_params
-        params.require(:user).permit(:email, :password, :password_confirmation)
+        params.require(:user).permit(:username, :email, :password, :password_confirmation)
     end
 
 end
