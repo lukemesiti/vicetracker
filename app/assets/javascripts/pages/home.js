44f8f47cafe2afc2486090c8506d1f7a4834267e
lukemesiti <lukemesiti@gmail.com>
2014-06-02 18:36:17 +1000

    remove redundant js

--- app/assets/javascripts/pages/home.js
+++ app/assets/javascripts/pages/home.js
@@ -1,28 +1,4 @@
 $(document).ready(function(){
     $('.carousel').carousel();
 
-    $(".money-description").mouseover(function() {
-      $(this).prev().addClass('money');
-    })
-
-    $(".money-description").mouseout(function() {
-      $(this).prev().removeClass('money');
-    })
-
-    $(".health-description").mouseover(function() {
-      $(this).prev().addClass('health');
-    })
-
-    $(".health-description").mouseout(function() {
-      $(this).prev().removeClass('health');
-    })
-
-    $(".track-description").mouseover(function() {
-      $(this).prev().addClass('track');
-    })
-
-    $(".track-description").mouseout(function() {
-      $(this).prev().removeClass('track');
-    })
-
 });
